1f42d967f27db5a5ce7ba9dc33c7c727e121210b
Vinn Nduri <ndurivo@gmail.com>
2022-06-05 21:50:33 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+
+
+const messages = [];
+
+function writeCards(name, event) {
+  for (let i = 0; i < name.length; i++) {
+   messages.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+   debugger;
+
+  }
+        return messages;
+}
+// writeCards(["Charlie", "Samip", "Ali"], "birthday");
+
+// console.log(writeCards(["Guadalupe", "Ollie", "Aki"], "birthday"));
+ function countDown(){
+
+
+let countdown = 10;
+while (countdown >= 0) {
+  console.log(countdown--);
+}
+}
+
